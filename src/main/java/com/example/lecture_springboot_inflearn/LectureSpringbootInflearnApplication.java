a39b2c749cd9002c093477d8fc003590a5e838b3
ayz1070 <ayz1070@gmail.com>
2025-01-12 09:00:19 +0900

    [init] 프로젝트 초기화
    = 프로젝트 초기설정 수행

---
+++ src/main/java/com/example/lecture_springboot_inflearn/LectureSpringbootInflearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecture_springboot_inflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureSpringbootInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureSpringbootInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lecture_springboot_inflearn/LectureSpringbootInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture_springboot_inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureSpringbootInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
