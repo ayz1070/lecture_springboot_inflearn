17a12f57f6b083c5e8e0a1ee5d54b5dceadfe561
Jun <80628496+ayz1070@users.noreply.github.com>
2025-02-01 14:43:11 +0900

    [session4] 영속성 관리 - 내부 동작 방식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,11 +15,11 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member1 = new Member(150L,"A");
-            Member member2 = new Member(160L,"B");
+            Member member = new Member(200L,"member200");
 
-            em.persist(member1);
-            em.persist(member2);
+            em.persist(member);
+
+            em.flush();
 
             System.out.println("==========================");
 
