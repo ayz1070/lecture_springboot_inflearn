f9f41c3a4245707ea391f091f8f37bfe6a941a35
Jun <80628496+ayz1070@users.noreply.github.com>
2025-02-03 13:42:50 +0900

    [session6] 연관관계 매핑 기초

--- src/main/java/com/example/lecture_springboot_inflearn/domain/Member.java
+++ src/main/java/com/example/lecture_springboot_inflearn/domain/Member.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter @Setter
 public class Member {
@@ -14,4 +17,8 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+    // 여기서의 member는 Order의 member
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 }
--- src/main/java/com/example/lecture_springboot_inflearn/domain/Order.java
+++ src/main/java/com/example/lecture_springboot_inflearn/domain/Order.java
@@ -3,6 +3,8 @@ package com.example.lecture_springboot_inflearn.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -10,10 +12,23 @@ public class Order {
     @Id @GeneratedValue
     private Long id;
 
-    @Column(name="MEMBER_ID")
-    private Long memberId;
+//    @Column(name="MEMBER_ID")
+//    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name="MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 }
